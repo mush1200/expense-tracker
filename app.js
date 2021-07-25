2614fba8ceb49f1690fe1a2038fba47f90f66ced
mush1200 <mush120@hotmail.com>
2021-07-25 10:45:05 +0800

    feat: add delete expense fun ction

--- app.js
+++ app.js
@@ -102,6 +102,15 @@ app.post('/records/:id/edit', (req, res) => {
     .then(() => res.redirect('/'))
     .catch(error => console.log(error))
 })
+
+//刪除
+app.post('/records/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Record.findById(id)
+    .then(record => record.remove())
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
 //監聽器
 app.listen(port, () => {
   console.log(`The Express server is running on http://localhost:${port}.`)
