ad6c3bb66a4d424ec89db91b2d7961d868983705
mush1200 <mush1200@hotmail.com>
2021-08-06 11:38:02 +0800

    feat: add user registeration feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,13 +1,38 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
+const { exists } = require('../../models/user')
 const router = express.Router()
-
+const User = require('../../models/user')
 router.get('/login', (req, res) => {
   res.render('login')
 })
 
 router.get('/register', (req, res) => {
   res.render('register')
+})
+
+router.post('/register', (req, res) => {
+  const { name, email, password, confirmPassword} = req.body
+  User.findOne({ email })
+    .then(user => {
+      if (user) {
+        console.log('User already exists.')
+        res.render('register', {
+          name,
+          email,
+          password,
+          confirmPassword
+        })
+      } else {
+        return User.create({
+          name,
+          email,
+          password
+        })
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err))
+      }
+    })
 })
 
 // 匯出路由器
