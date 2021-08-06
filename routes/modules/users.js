ef2577a400d1616daf4f6dfa0f4c7ed699b8696f
mush1200 <mush1200@hotmail.com>
2021-08-06 12:19:02 +0800

    feat: add logout feature

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -40,5 +40,10 @@ router.post('/register', (req, res) => {
     })
 })
 
+//登出
+router.get('/logout', (req, res) => {
+  req.logout()
+  res.redirect('/users/login')
+})
 // 匯出路由器
 module.exports = router
